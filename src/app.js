62fcf2a499f64fb3e7ba8d2d6d5d73058fb83d9b
tree-adams <106931905+tree-adams@users.noreply.github.com>
2022-09-19 11:18:56 -0700

    wip

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors")
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
-
+// const corsOption = {
+//     origin: ['https://localhost:3000']
+// }
 app.use(express.json());
+// app.use(cors(corsOption));
+app.use(cors());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
